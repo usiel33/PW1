4ac5c0d1b0a743025b402c94b503b7a7ff16743b
usiel33 <uquispep@unsa.edu.pe>
2024-10-30 21:15:04 -0500

    Se actualiza el lab 6

--- Laboratorios/QUISPE_PUMA_LABORATAORIO_05/cgi/busquedas.pl
+++ Laboratorios/QUISPE_PUMA_LABORATAORIO_05/cgi/busquedas.pl
@@ -1,57 +1,61 @@
 #!/usr/bin/perl
-
 use strict;
 use warnings;
-use CGI qw(:standard);
-use Text::CSV;
+use CGI ':standard';
 
-# Crear el objeto CGI
-my $cgi = CGI->new;
+print header('text/html');
+print start_html('Resultados de Búsqueda');
 
-# Definir la cabecera de la respuesta
-print $cgi->header('text/html; charset=UTF-8');
+# Obtener parámetros de la URL
+my $nombre = param('nombre') || '';
+my $departamento = param('departamento') || '';
 
-# Obtener los parámetros del formulario
-my $nombre_universidad = $cgi->param('nombre_universidad') // '';
-my $periodo_licenciamiento = $cgi->param('periodo_licenciamiento') // '';
-my $departamento_local = $cgi->param('departamento_local') // '';
-my $denominacion_programa = $cgi->param('denominacion_programa') // '';
+# Definir las columnas para extraer del CSV
+my @columnas = (
+    "NOMBRE",                 # Nombre Universidad
+    "PERIODO_LICENCIAMIENTO", # Periodo Licenciamiento
+    "DEPARTAMENTO_LOCAL",     # Departamento Local
+    "DENOMINACION_PROGRAMA"   # Denominación Programa
+);
 
-# Cargar y procesar el archivo CSV
-my $file = 'Programas de Universidades.csv'; # Ruta al archivo CSV
-my @resultados;
+# Leer el archivo CSV
+open my $file, '<', '/usr/local/apache2/htdocs/data/universidades.csv' or die "No se puede abrir el archivo: $!";
+my $header = <$file>;
 
-# Usar Text::CSV para leer el archivo
-my $csv = Text::CSV->new({ binary => 1, auto_diag => 1 });
-
-open my $fh, '<:encoding(utf8)', $file or die "No se puede abrir '$file': $!";
-while (my $row = $csv->getline($fh)) {
-    my ($nombre, $periodo, $departamento, $denominacion) = @$row;
-
-    # Filtrar los resultados según las consultas
-    if (($nombre_universidad eq '' || $nombre =~ /$nombre_universidad/i) &&
-        ($periodo_licenciamiento eq '' || $periodo == $periodo_licenciamiento) &&
-        ($departamento_local eq '' || $departamento =~ /$departamento_local/i) &&
-        ($denominacion_programa eq '' || $denominacion =~ /$denominacion_programa/i)) {
-        push @resultados, $row; # Agregar el resultado a la lista
-    }
-}
-close $fh;
-
-# Mostrar los resultados
-print $cgi->start_html('Resultados de la Búsqueda');
-print $cgi->h1('Resultados de la Búsqueda');
-
-if (@resultados) {
-    print $cgi->start_table;
-    print $cgi->Tr($cgi->th(['Nombre Universidad', 'Periodo Licenciamiento', 'Departamento Local', 'Denominación Programa']));
-    foreach my $resultado (@resultados) {
-        print $cgi->Tr($resultado);
-    }
-    print $cgi->end_table;
-} else {
-    print $cgi->p('No se encontraron resultados.');
+# Identificar índices de las columnas
+my %col_indices;
+my @headers = split /\|/, $header;
+for my $i (0..$#headers) {
+    $col_indices{$headers[$i]} = $i;
 }
 
-print $cgi->a({ href => '../index.html' }, 'Volver a buscar');
-print $cgi->end_html;
+# Validar que las columnas necesarias existen
+for my $col (@columnas) {
+    die "Columna $col no encontrada en el archivo" unless exists $col_indices{$col};
+}
+
+# Imprimir resultados de la tabla
+print "<table border='1'><tr><th>Nombre Universidad</th><th>Periodo Licenciamiento</th><th>Departamento Local</th><th>Denominación Programa</th></tr>";
+
+while (my $line = <$file>) {
+    chomp $line;
+    my @fields = split /\|/, $line;
+
+    # Aplicar filtros si están definidos
+    next if $nombre && $fields[$col_indices{'NOMBRE'}] !~ /\Q$nombre\E/i;
+    next if $departamento && $fields[$col_indices{'DEPARTAMENTO_LOCAL'}] !~ /\Q$departamento\E/i;
+
+    # Imprimir filas
+    print "<tr>";
+    print "<td>$fields[$col_indices{'NOMBRE'}]</td>";
+    print "<td>$fields[$col_indices{'PERIODO_LICENCIAMIENTO'}]</td>";
+    print "<td>$fields[$col_indices{'DEPARTAMENTO_LOCAL'}]</td>";
+    print "<td>$fields[$col_indices{'DENOMINACION_PROGRAMA'}]</td>";
+    print "</tr>";
+}
+
+
+print "</table>";
+print end_html;
+
+close $file;
